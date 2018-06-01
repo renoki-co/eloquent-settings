2ff57e5e22014b2b11dfe2f1fa42d8eabde12902
Alex Renoki <rennokki@gmail.com>
2018-06-01 10:56:51 +0300

    Setting up files for testing and models.

---
+++ tests/TestCase.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Rennokki\Settings\Test;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+
+use Rennokki\Settings\Test\Models\User;
+use Rennokki\Settings\Models\SettingModel;
+
+abstract class TestCase extends Orchestra
+{
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->resetDatabase();
+
+        $this->loadLaravelMigrations();
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->withFactories(__DIR__.'/../database/factories');
+
+        $this->artisan('migrate', ['--database' => 'sqlite']);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Rennokki\Settings\SettingsServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => __DIR__.'/database.sqlite',
+            'prefix' => '',
+        ]);
+        $app['config']->set('auth.providers.users.model', User::class);
+        $app['config']->set('app.key', 'wslxrEFGWY6GfGhvN9L3wH3KSRJQQpBD');
+        $app['config']->set('eloquent-settings.model', SettingModel::class);
+    }
+
+    protected function resetDatabase()
+    {
+        file_put_contents(__DIR__.'/database.sqlite', null);
+    }
+}
