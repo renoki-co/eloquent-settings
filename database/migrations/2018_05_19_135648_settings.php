7bb20c2150a49b08b7efa0e6c4a9d3666561ef96
Alex Renoki <rennokki@gmail.com>
2018-06-15 11:19:19 +0300

    interger caused errors.

--- database/migrations/2018_05_19_135648_settings.php
+++ database/migrations/2018_05_19_135648_settings.php
@@ -22,7 +22,7 @@ class Settings extends Migration
 
             $table->string('key', 100);
             $table->text('value')->nullable();
-            $table->enum('cast_type', ['string', 'interger', 'int', 'boolean', 'bool', 'float', 'double'])->default('string');
+            $table->enum('cast_type', ['string', 'integer', 'int', 'boolean', 'bool', 'float', 'double'])->default('string');
 
             $table->timestamps();
 
