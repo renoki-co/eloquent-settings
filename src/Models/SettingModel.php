84ceb49ce2067554a8bde2a43936b24a4a198f3a
rennokki <rennokki@gmail.com>
2018-06-15 09:59:10 +0300

    Adding cast_type to Model

--- src/Models/SettingModel.php
+++ src/Models/SettingModel.php
@@ -8,7 +8,7 @@ class SettingModel extends Model
 {
     protected $table = 'settings';
     protected $fillable = [
-        'model_id', 'model_type', 'key', 'value',
+        'model_id', 'model_type', 'key', 'value', 'cast_type',
     ];
 
     public function model()
