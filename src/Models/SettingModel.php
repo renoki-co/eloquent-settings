7a6b84dd4abf2e7555eb965af37dbe9795f24d2a
Alex Renoki <rennokki@gmail.com>
2018-09-04 19:02:01 +0300

    Using $guarded instead of $fillable.

--- src/Models/SettingModel.php
+++ src/Models/SettingModel.php
@@ -7,9 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class SettingModel extends Model
 {
     protected $table = 'settings';
-    protected $fillable = [
-        'model_id', 'model_type', 'key', 'value', 'cast_type',
-    ];
+    protected $guarded = [];
 
     public function model()
     {
